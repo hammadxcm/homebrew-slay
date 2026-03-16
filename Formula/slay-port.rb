320169b3ba23f03fe1b3b227896129ffb11777b0
hammadxcm <hammadkhanxcm@gmail.com>
2026-03-16 16:12:47 +0500

    Update slay-port to 0.1.2

--- Formula/slay-port.rb
+++ Formula/slay-port.rb
@@ -2,25 +2,25 @@ class SlayPort < Formula
   desc "Kill processes by port. Beautifully."
   homepage "https://slay.fyniti.co.uk"
   license "MIT"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/hammadxcm/slay/releases/download/v#{version}/slay-darwin-arm64"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/hammadxcm/slay/releases/download/v0.1.2/slay-darwin-arm64"
+      sha256 "2c0fbbfe54e912ed33e4b9ea7b337e29ec29e3672bbb997f167bd35ebe5ca6db"
     else
-      url "https://github.com/hammadxcm/slay/releases/download/v#{version}/slay-darwin-x64"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/hammadxcm/slay/releases/download/v0.1.2/slay-darwin-x64"
+      sha256 "39f78405ed47c9f2b939ee73b0b12760158120f01bdb49e583f98ac97666c6e8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/hammadxcm/slay/releases/download/v#{version}/slay-linux-arm64"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/hammadxcm/slay/releases/download/v0.1.2/slay-linux-arm64"
+      sha256 "1838729949284ee7bbd644aa0fcb76914b2502eb8d9d899609c4ad17a8e0b796"
     else
-      url "https://github.com/hammadxcm/slay/releases/download/v#{version}/slay-linux-x64"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/hammadxcm/slay/releases/download/v0.1.2/slay-linux-x64"
+      sha256 "17087f5e20bc5c4d136f8461eaa100dbcc1a71a4e0d499846871382ab5b85a85"
     end
   end
 
